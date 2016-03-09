6693acbe66be4156c5b7f78f145fbc566c33c470
apneljo <spads@live.dk>
2016-03-09 13:26:12 +0100

    Did some weird shizzle

---
+++ BlogAndPost/BlogAndPost/Model1.cs
@@ -0,0 +1,22 @@
+namespace BlogAndPost
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=BlogAndPost")
+        {
+        }
+
+        public virtual DbSet<Blogs> Blogs { get; set; }
+        public virtual DbSet<Posts> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ BlogAndPost/BlogAndPost/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAndPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Create Blog\n2.CreatePost\n3.List All blogs\n4.List specific blog\n0.End\nPlease enter your choice:");
+            int choice = int.Parse(Console.ReadLine());
+            Console.Clear();
+            while (choice != 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        CreateBlog();
+                        break;
+                    case 2:
+                        CreatePost();
+                        break;
+                    case 3:
+                        ListAllBlogs();
+                        break;
+                    case 4:
+                        ListSpecificBlog();
+                        break;
+                }
+                Console.WriteLine(
+                    "1.Create Blog\n2.CreatePost\n3.List All blogs\n4.List specific Blog\n0.End\nPlease enter your choice:");
+                choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void ListSpecificBlog()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ListAllBlogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreatePost()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateBlog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
